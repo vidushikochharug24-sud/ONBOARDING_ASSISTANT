87e9e1083530b848406b85a749b98f66f0c6d780
Vidushi Kochhar <vidushi.kochhar.ug24@nsut.ac.in>
2026-06-26 03:32:53 +0530

    Save recent changes

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 ﻿import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://onboarding-assistant-3.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
